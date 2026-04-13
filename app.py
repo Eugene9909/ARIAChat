d4749a4b50339db3b4e0e73cdcb983c51912b18f
DopestTizzy <92105050+DopestTizzy@users.noreply.github.com>
2026-04-13 05:48:37 +0200

    fixing cors

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 @app.after_request
-def add_cors_headers(response):
+def cors(response):
     response.headers["Access-Control-Allow-Origin"] ="https://aria-chatapp.vercel.app"
-    response.headers["Access-Control-Allow-Credential"] ="true"
     response.headers["Access-Control-Allow-Headers"] ="Content-Type"
     response.headers["Access-Control-Allow-Methods"] ="GET, POST,  OPTIONS"
+    response.headers["Access-Control-Allow-Credentials"] ="true"
     return response
-CORS(app, supports_credentials=True, origins=["https://aria-chatapp.vercel.app"])
+#CORS(app, supports_credentials=True, origins=["https://aria-chatapp.vercel.app"])
 
 load_dotenv()
 
